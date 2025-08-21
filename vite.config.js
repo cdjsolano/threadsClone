36b5f5c2971606be0759158f9fbb2b60d2473220
Celso Solano <cdjsolano@gmai.com>
2025-08-20 23:54:34 -0500

    fix: add missing react-toastify dependency 4

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true
-  },
+
   build: {
      optimizeDeps: {
        include: ['lucide-react', 'react-toastify']
-     }
+     },
+      rollupOptions: {
+      input: './index.html'
+    }
   }
 })
