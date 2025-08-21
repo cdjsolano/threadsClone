d8f29bba2633ad41c7137d5894cad39167c2f5a0
Celso Solano <cdjsolano@gmai.com>
2025-08-21 01:49:32 -0500

    fix: lock lucide-react stable version

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  build: {
-    optimizeDeps: {
-      include: ['lucide-react']
-    },
+  optimizeDeps: {
+    include: ['lucide-react'], // aquí sí lo toma en cuenta
+  },
     rollupOptions: {
       input: './index.html',
       external: ['react-toastify',
         'react-toastify/dist/ReactToastify.css']
     }
-  }
+  
 })
