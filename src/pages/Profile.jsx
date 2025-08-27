a43e2fe2de36ab2e8122ca42b4c1d2d812c80775
Celso Solano <cdjsolano@gmai.com>
2025-08-27 15:43:23 -0500

    agregar modulo para seguir usuarios 2

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { supabase } from "../../supabaseClient";
 import { Post } from "../components/Feed/Post";
